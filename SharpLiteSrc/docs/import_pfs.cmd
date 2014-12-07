3ee978b2081d6dc7dcee5b63d2225c84994b468d
ivan <ivan@boxfusion.co.za>
2014-12-07 13:00:13 +0300

    updated packages

---
+++ SharpLiteSrc/docs/import_pfs.cmd
@@ -0,0 +1,5 @@
+"c:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools\sn.exe" -i SharpLite.pfx VS_KEY_2B7160AFD7782FEA
+"c:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools\sn.exe" -i SharpLite.pfx VS_KEY_36BBEBBFDF625271
+"c:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools\sn.exe" -i SharpLite.pfx VS_KEY_5CF8969BCE936910
+"c:\Program Files (x86)\Microsoft SDKs\Windows\v8.0A\bin\NETFX 4.0 Tools\sn.exe" -i SharpLite.pfx VS_KEY_E17CCD6B2B5E87D1
+pause
